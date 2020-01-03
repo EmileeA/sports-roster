4365c3c74995617b668e5951469c7b20746c0e5a
Emilee Mitchell <emcakes@gmail.com>
2020-01-02 20:31:31 -0600

    WIP

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import './App.scss';
+import NavBar from '../components/NavBar/NavBar.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../../images/logo.png';
 
+function App() {
+  return(
+    <div className="App"><NavBar />
+      <div className="authContainer">
+      <img className="logo" src = $ {logo}/>
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
